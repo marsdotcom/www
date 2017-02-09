860d8031945bb5c69231dab13c5ed4a391c74a45
marsdotcom <mars.exe@live.com>
2017-02-09 23:02:05 +0300

    add deleting

--- cabinet.js
+++ cabinet.js
@@ -1,184 +1,220 @@
  document.getElementById('a').onclick = function (){               
-             var date = new Date(0);
-             document.cookie = "log=; path=/; expires=" + date.toUTCString();
-           }   
+ 	var date = new Date(0);
+ 	document.cookie = "log=; path=/; expires=" + date.toUTCString();
+ }   
  
  var workers = document.getElementById('workers');
  
  document.getElementById('rab').onclick = function(){     
-     
-     workers.classList.remove('invisible');
-     
-     
+
+ 	workers.classList.remove('invisible');
+
+
  }
  
  document.getElementById('closewokers').onclick = function(){
-     
-        workers.classList.add('invisible');   
-     
+
+ 	workers.classList.add('invisible');   
+
  }
  
  
  function getXmlHttp(){
-		var xmlhttp;
-		try {
-			xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch (e) {
-			try {
-				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (E) {
-				xmlhttp = false;
-			}
-		}
-		if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
-			xmlhttp = new XMLHttpRequest();
-		}
-		return xmlhttp;
-	}
+ 	var xmlhttp;
+ 	try {
+ 		xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+ 	} catch (e) {
+ 		try {
+ 			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+ 		} catch (E) {
+ 			xmlhttp = false;
+ 		}
+ 	}
+ 	if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+ 		xmlhttp = new XMLHttpRequest();
+ 	}
+ 	return xmlhttp;
+ }
 
 
-var httpreq = getXmlHttp();
+ var httpreq = getXmlHttp();
 
-var bsubmit = document.getElementById('submit');
+ var bsubmit = document.getElementById('submit');
 
-bsubmit.onclick = click;
-    
-    
-    function click(event){
-                            var res = document.getElementById('res'),
-                            fam = document.getElementsByName('fam')[0].value,
-                            nam = document.getElementsByName('name')[0].value,
-                            nic = document.getElementsByName('nic')[0].value,
-                            tel = document.getElementsByName('tel')[0].value,
-                            mail = document.getElementsByName('mail')[0].value;
-    
-    var body = "fam="+fam+"&name="+nam+"&nic="+nic+"&tel="+tel+"&mail="+mail+"&form=workers";
-    
-        sendData(body);
-    
-}
+ bsubmit.onclick = click;
 
 
-function sendData(body){
-    httpreq.open('POST', "fhandler.php", true);
-		httpreq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        
-		httpreq.onreadystatechange = function() {
-			if (httpreq.readyState == 4) {
-				if(httpreq.status == 200) {
-					res.innerHTML = httpreq.responseText;          
-				}
-					
-			}
-		}
-		httpreq.send(body);
-}
+ function click(event){
+ 	var res = document.getElementById('res'),
+ 	fam = document.getElementsByName('fam')[0].value,
+ 	nam = document.getElementsByName('name')[0].value,
+ 	nic = document.getElementsByName('nic')[0].value,
+ 	tel = document.getElementsByName('tel')[0].value,
+ 	mail = document.getElementsByName('mail')[0].value;
 
-var table = document.getElementById('table');
+ 	var body = "fam="+fam+"&name="+nam+"&nic="+nic+"&tel="+tel+"&mail="+mail+"&form=workers";
+
+ 	addData(body);
+
+ }
 
 
-document.getElementById('rabshow').onclick = function (){
-    
-    var body = "table=workers";
-    
-    getData(body);    
-    
-}
+ function addData(body){
+ 	httpreq.open('POST', "fhandler.php", true);
+ 	httpreq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-function getData(body){
-    httpreq.open('POST', "gettable.php", true);
-		httpreq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        
-		httpreq.onreadystatechange = function() {
-			if (httpreq.readyState == 4) {
-				if(httpreq.status == 200) {
-					table.innerHTML = httpreq.responseText;          
-                    initializebbb();
-				}
-					
-			}
-		}
-		httpreq.send(body);
-}
+ 	httpreq.onreadystatechange = function() {
+ 		if (httpreq.readyState == 4) {
+ 			if(httpreq.status == 200) {
+ 				res.innerHTML = httpreq.responseText;        
+ 				getData("table=workers"); 			// =====   получение таблицы, для показа записей после удаления  
+ 			}
 
-function initializebbb(){
-    
-    var bbbs = document.getElementsByName('bbb');
-    
-    for (var i=0;i<bbbs.length;i++){
-        
-        bbbs[i].onclick = bbbclick;
-        
-    }
-    
-}
+ 		}
+ 	}
+ 	httpreq.send(body);
+ }
 
-function bbbclick()
-{
-    var id = this.value; 
-    getidDate("id="+id+"&table=workers");
-    document.getElementsByName('id')[0].value = id;    
-}
+ var table = document.getElementById('table');
 
-var bupdate = document.getElementById('update');
-bupdate.onclick=function(){
-    
-    var res = document.getElementById('res'),
-    fam = document.getElementsByName('fam')[0].value,
-    nam = document.getElementsByName('name')[0].value,
-    nic = document.getElementsByName('nic')[0].value,
-    tel = document.getElementsByName('tel')[0].value,
-    mail = document.getElementsByName('mail')[0].value,
-    id = document.getElementsByName('id')[0].value;
-    
-    var body = "fam="+fam+"&name="+nam+"&nic="+nic+"&tel="+tel+"&mail="+mail+"&form=workers&id="+id;
-    
-    upDate(body);
-    
-}
 
-function getidDate(body){
-    
-    httpreq.open('POST', "gettable.php", true);
-		httpreq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        
-		httpreq.onreadystatechange = function() {
-			if (httpreq.readyState == 4) {
-				if(httpreq.status == 200) {
-					document.getElementById('datadiv').innerHTML = httpreq.responseText; 
-                    workers.classList.remove('invisible');
-                    bupdate.classList.remove('invisible'); 
-                    bsubmit.classList.add('invisible');
-				}
-					
-			}
-		}
-		httpreq.send(body);   
-    
-}
+ document.getElementById('rabshow').onclick = function (){
 
-function upDate(body){
-    
-    httpreq.open('POST', "fhandler.php", true);
-		httpreq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        
-		httpreq.onreadystatechange = function() {
-			if (httpreq.readyState == 4) {
-				if(httpreq.status == 200) {
-					res.innerHTML = httpreq.responseText; 
-                    getData("table=workers");
-                    bsubmit.classList.remove('invisible'); 
-                    bupdate.classList.add('invisible');
-                    document.getElementsByName('id')[0].value = null;
-                     fam = document.getElementsByName('fam')[0].value=null;
-                    document.getElementsByName('name')[0].value=null;
-                    document.getElementsByName('nic')[0].value=null;
-                    document.getElementsByName('tel')[0].value=null;
-                    document.getElementsByName('mail')[0].value=null;
-				}
-					
-			}
-		}
-		httpreq.send(body);   
-    
-}
+ 	var body = "table=workers";
+
+ 	getData(body);    
+
+ }
+
+ function getData(body){
+ 	httpreq.open('POST', "gettable.php", true);
+ 	httpreq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+ 	httpreq.onreadystatechange = function() {
+ 		if (httpreq.readyState == 4) {
+ 			if(httpreq.status == 200) {
+ 				table.innerHTML = httpreq.responseText;          
+ 				initializebbb();
+ 			}
+
+ 		}
+ 	}
+ 	httpreq.send(body);
+ }
+
+ function initializebbb(){
+
+ 	var bds = document.getElementsByClassName("bd");
+
+ 	for (var i=0;i<bds.length;i++){
+
+ 		bds[i].onclick = bdclick;
+
+ 	}
+
+ }
+
+ function bdclick()
+ {
+ 	var id = this.value; 
+ 	var name = this.getAttribute('name');
+
+ 	if (name==='bbb'){
+ 		getidDate("id="+id+"&table=workers");
+ 		document.getElementsByName('id')[0].value = id;   
+ 	}
+
+ 	if (name === 'ddd') {
+ 		if (confirm("Вы уверены в этом?")) {
+ 			delDate("id="+id+"&del=del");
+ 		}
+ 	}
+ }
+
+
+ var bupdate = document.getElementById('update');
+
+ bupdate.onclick=function(){   						//====== Изменение записи ====== 
+
+ 	var res = document.getElementById('res'),
+ 	fam = document.getElementsByName('fam')[0].value,
+ 	nam = document.getElementsByName('name')[0].value,
+ 	nic = document.getElementsByName('nic')[0].value,
+ 	tel = document.getElementsByName('tel')[0].value,
+ 	mail = document.getElementsByName('mail')[0].value,
+ 	id = document.getElementsByName('id')[0].value;
+
+ 	var body = "fam="+fam+"&name="+nam+"&nic="+nic+"&tel="+tel+"&mail="+mail+"&form=workers&id="+id;
+
+ 	upDate(body);
+
+ }
+
+ function delDate(body){
+
+ 	httpreq.open('POST', "fhandler.php", true);
+ 	httpreq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+ 	httpreq.onreadystatechange = function() {
+ 		if (httpreq.readyState == 4) {
+ 			if(httpreq.status == 200) {
+
+ 				var result = httpreq.responseText;
+
+ 				alert(result);
+
+ 				getData("table=workers"); 			// =====   получение таблицы, для показа записей после удаления
+ 			}
+
+ 		}
+ 	}
+ 	httpreq.send(body);   
+
+ }
+
+
+ function getidDate(body){						// ========= получение записи по id и отображанение в форме ввода для изменения
+
+ 	httpreq.open('POST', "gettable.php", true);
+ 	httpreq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+ 	httpreq.onreadystatechange = function() {
+ 		if (httpreq.readyState == 4) {
+ 			if(httpreq.status == 200) {
+ 				document.getElementById('datadiv').innerHTML = httpreq.responseText; 
+ 				workers.classList.remove('invisible');
+ 				bupdate.classList.remove('invisible'); 
+ 				bsubmit.classList.add('invisible');
+ 			}
+
+ 		}
+ 	}
+ 	httpreq.send(body);   
+
+ }
+
+ function upDate(body){  						//====== обновление записи ========
+
+ 	httpreq.open('POST', "fhandler.php", true);
+ 	httpreq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+ 	httpreq.onreadystatechange = function() {
+ 		if (httpreq.readyState == 4) {
+ 			if(httpreq.status == 200) {
+ 				res.innerHTML = httpreq.responseText; 
+ 				getData("table=workers");
+ 				bsubmit.classList.remove('invisible'); 
+ 				bupdate.classList.add('invisible');
+ 				document.getElementsByName('id')[0].value = null;
+ 				document.getElementsByName('fam')[0].value=null;
+ 				document.getElementsByName('name')[0].value=null;
+ 				document.getElementsByName('nic')[0].value=null;
+ 				document.getElementsByName('tel')[0].value=null;
+ 				document.getElementsByName('mail')[0].value=null;
+ 			}
+
+ 		}
+ 	}
+ 	httpreq.send(body);   
+
+ }
